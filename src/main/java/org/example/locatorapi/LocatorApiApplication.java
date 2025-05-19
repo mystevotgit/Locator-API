e8a97d639eef0b3c3067246ade876a453f160023
olatunjioye <oyeolatunji@gmail.com>
2025-05-19 14:47:49 -0500

    initial commit

---
+++ src/main/java/org/example/locatorapi/LocatorApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.locatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocatorApiApplication.class, args);
+    }
+
+}
